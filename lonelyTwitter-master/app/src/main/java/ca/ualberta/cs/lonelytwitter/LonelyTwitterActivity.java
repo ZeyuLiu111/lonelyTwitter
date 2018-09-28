b8aa68b318df3d6a08a64ded76ae0ec45b917f52
Zeyu Liu <35505890+ZeyuLiu111@users.noreply.github.com>
2018-09-28 15:05:02 -0600

    Add files via upload

--- lonelyTwitter-master/app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ lonelyTwitter-master/app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,11 +1,14 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,12 +21,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
+	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+	private ArrayAdapter<Tweet> adapter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,11 @@ public class LonelyTwitterActivity extends Activity {
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				ImportantTweet newTweet = new ImportantTweet(text);
+				tweets.add(newTweet);
+				adapter.notifyDataSetChanged();
+				saveInFile();
 
 			}
 		});
@@ -48,42 +58,54 @@ public class LonelyTwitterActivity extends Activity {
 
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
-		oldTweetsList.setAdapter(adapter);
+
+		loadFromFile();
+		if(tweets.size()>0) {
+			adapter = new ArrayAdapter<Tweet>(this,
+					R.layout.list_item, tweets);
+			oldTweetsList.setAdapter(adapter);
+		}
+
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
+
 		try {
-			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
+			FileInputStream fis=openFileInput(FILENAME);
+			InputStreamReader isr=new InputStreamReader(fis);
+			BufferedReader reader=new BufferedReader(isr);
+
+			Gson gson = new Gson();
+			Type typeListTweets=new TypeToken<ArrayList<ImportantTweet>>(){}.getType();
+			tweets=gson.fromJson(reader,typeListTweets);
+
 			}
 
-		} catch (FileNotFoundException e) {
+		 catch (FileNotFoundException e) {
+			tweets=new ArrayList<Tweet>();
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		//return tweets.toArray(new String[tweets.size()]);
 	}
 	
-	private void saveInFile(String text, Date date) {
+	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
+
+		    FileOutputStream fos=openFileOutput(FILENAME,
+					0);
+
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			BufferedWriter writer= new BufferedWriter(osw);
+			Gson gson=new Gson();
+			gson.toJson(tweets,writer);
+			writer.flush();
+			writer.close();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ lonelyTwitter-master/app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,45 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public abstract class Tweet{
+
+    protected Date date;
+    protected String message;
+    private static final Integer MAX_CHARS = 140;
+
+    //Empty argument constructor with default values
+    Tweet() {
+        //Must use the 'this' keyword in order to specify the current object message = message does nothing!
+        this.date = new Date();
+        this.message = "I am default message schwa!";
+    }
+
+    //Overloading: so that we can specify the tweet content
+    public Tweet(String message) {
+        this.message = message;
+        this.date = new Date(System.currentTimeMillis());
+
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) throws TweetTooLongException {
+        if (message.length() <= this.MAX_CHARS ) {
+            this.message = message;
+        } else {
+            throw new TweetTooLongException();
+        }
+    }
+
+    public Date getDate() { return this.date; }
+
+    //No method body implemented! We leave that up to the subclasses (they MUST implement it)
+    public abstract Boolean isImportant();
+
+    public String toString(){
+        return this.message;
+    }
+}
